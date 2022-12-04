d65e64ceb00f7ef55e4acb5f6e932f663db00142
Farhan-4697 <Patelfarhan431@gmail.com>
2022-12-04 20:30:43 +0530

    commit

---
+++ src/Details.js
@@ -0,0 +1,180 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/ui/logo2.svg";
+import logo from "./assets/ui/logo2.svg";
+// Profile Image
+import profile from "./assets/ui/profile.png";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Farhan Patel",
+  tagline: "I build things for web",
+  img: profile,
+  about: `I am an Engineering Student on track to graduate in summer 2023 with Btech in Computer Science(CGPA - 7.75) from Sanjay Ghodawat University. I had opportunity to work for Revoluiton IT Solutions as a Intern Front-end Developer and Currently working as Intern Mern Stack Developer at iGAP Technologies.
+  .`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/farhan-patel-31981120b/", 
+  twitter: "https://twitter.com/Mt4697",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Intern Frontend Web Developer",
+    Company: `Revolution IT Solutions`,
+    Location: "Kolhapur",
+    Type: "Internship",
+    Duration: "Dec 2021 - Jan 2021",
+  },
+  {
+    Position: "Intern MERN Stack Developer",
+    Company: `iGAP Technologies`,
+    Location: "Kolhapur",
+    Type: "Internship",
+    Duration: "Jul 2022 - Present",
+  },
+  // {
+  //   Position: "Internship",
+  //   Company: `Company Name here`,
+  //   Location: "Bengaluru",
+  //   Type: "Internship",
+  //   Duration: "Sep 2021 - Dec 2021",
+  // },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "B.tech in Computer Science Engineering",
+    Company: `Sanjay Ghodawat University`,
+    Location: "Kolhapur",
+    Type: "Full Time",
+    Duration: "Jul 2019 - Present",
+  },
+  // {
+  //   Position: "B.tech in Computer Science Engineering",
+  //   Company: `Sanjay Ghodawat University`,
+  //   Location: "Kolhapur",
+  //   Type: "Full Time",
+  //   Duration: "Jul 2019 - Present",
+  // },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+   tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+   git: git,
+  github: github,
+   figma: github,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "VJP IIT Academy",
+    image: projectImage1,
+    description: `It is a Website based on Html , Css , Bootstrap , Javascript created for vjp iit academy for there business purpose.`,
+    techstack: "HTML/CSS, Bootstrap, JavaScript",
+    previewLink: "http://www.vjpima.in/",
+    githubLink: "https://github.com",
+    
+  },
+  {
+    title: "CryptoCurrency Market Cap",
+    image: projectImage2,
+    description: `It's a website which shows Cryptocurrency Market cap which is based on React js and using Public Api`,
+    techstack: "HTML/CSS, JavaScript , React",
+    previewLink: "https://soft-crisp-469164.netlify.app/",
+    githubLink: "https://github.com/Farhan-4697/Crypto-master",
+  },
+//   {
+//     title: "Project title 3",
+//     image: projectImage3,
+//     description: `This is sample project description random things are here in description This is sample
+// project lorem ipsum generator for dummy content`,
+//     techstack: "HTML/CSS, JavaScript",
+//     previewLink: "https://ubiquitous-crepe-0e6a1b.netlify.app/",
+//     githubLink: "https://github.com/Farhan-4697/React-Weather-App-main",
+//   },
+  {
+    title: "To-Do List",
+    image: projectImage3,
+    description: `Its a T0-Do list app where you can add update and delete your day to day tasks created using React Js`,
+    techstack: "HTML/CSS, JavaScript , React",
+    previewLink: "https://fantastic-rolypoly-4100d5.netlify.app//",
+    githubLink: "https://github.com/Farhan-4697/react-to-do-list-master",
+  },
+  {
+    title: "Weather App",
+    image: projectImage4,
+    description: `Weather app shows your daily weather conditions of city you search created using React js and public Api`,
+    techstack: "HTML/CSS, JavaScript , React",
+    previewLink: "https://ubiquitous-crepe-0e6a1b.netlify.app/",
+    githubLink: "https://github.com/Farhan-4697/React-Weather-App-main",
+  }
+//   {
+//     title: "Project title 5",
+//     image: projectImage5,
+//     description: `This is sample project description random things are here in description This is sample
+// project lorem ipsum generator for dummy content`,
+//     techstack: "HTML/CSS, JavaScript",
+//     previewLink: "https://google.com",
+//     githubLink: "https://github.com",
+//   },
+//   {
+//     title: "Project title 6",
+//     image: projectImage6,
+//     description: `This is sample project description random things are here in description This is sample
+// project lorem ipsum generator for dummy content`,
+//     techstack: "HTML/CSS, JavaScript",
+//     previewLink: "https://google.com",
+//     githubLink: "https://github.com",
+//   },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "patelfarhan431@gmail.com",
+  phone: "+91 9284705143",
+};
